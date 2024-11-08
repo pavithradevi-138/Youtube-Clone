4a8662280ebd4647f9c3dc6900a10b690e2e4562
pavithradevi-138 <pavithradevi138.dev@gmail.com>
2024-11-08 07:26:48 +0530

    Youtube Clone - Navbar created

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import "./Navbar.css";
+import menu from "../../assets/menu.png";
+import logo from "../../assets/logo.png";
+import search from "../../assets/search.png";
+import upload from "../../assets/upload.png";
+import more from "../../assets/more.png";
+import notification from "../../assets/notification.png";
+import jack from '../../assets/jack.png'
+
+const Navbar = () => {
+  return (
+    <nav className="flex-div">
+      <div className="nav-left flex-div">
+        <img src={menu} alt="menu icon" className="menu-icon" />
+        <img src={logo} alt="logo img" className="logo" />
+      </div>
+
+      <div className="nav-middle flex-div">
+        <div className="search-box flex-div">
+          <input type="text" name="search" placeholder="Search" />
+          <img src={search} alt="search icon" />
+        </div>
+      </div>
+
+      <div className="nav-right flex-div">
+        <img src={upload} alt="upload icon" />
+        <img src={more} alt="more icon" />
+        <img src={notification} alt="notification icon" />
+        <img src={jack} alt="profile img" className="user-icon" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
